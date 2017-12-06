2a249c663d3123837d0ada6943cbe2ae806e7562
EugeneKrotyukov <krotyukov@gmail.com>
2017-12-06 16:26:34 +0200

    refactor: del func write_to_file

--- bd_olx.py
+++ bd_olx.py
@@ -6,13 +6,13 @@ def create_bd(bd_name):
     with conn:
         cursor = conn.cursor()
         cursor.execute('''DROP TABLE IF EXISTS olx_parsing''') # удаляет таблицу, если она существует
-        cursor.execute('''CREATE TABLE olx_parsing ad_number real, title text, price real, date text, place text, phone text, context text''')
+        cursor.execute('''CREATE TABLE olx_parsing (ad_number real, title text, price real, date text, place text, phone text, content text)''')
 
-def insert_bd(bd_name, ad_number, title, price, date, place, phone, context):
+def insert_bd(bd_name, ad_number, title, price, date, place, phone, content):
     conn = sqlite3.connect(bd_name)
     with conn:
         cursor = conn.cursor()
-        cursor.executemany('''INSERT INTO olx_parsing (order, ad_number, title, price, date, place, person, condition, phone, context)
+        cursor.executemany('''INSERT INTO olx_parsing (order, ad_number, title, price, date, place, person, condition, phone, content)
                             VALUE(?, ?, ?, ?, ?, ?, ?)''',
-                           (ad_number, title, price, date, place, phone, context))
+                           (ad_number, title, price, date, place, phone, content))
 
--- scrape_olx_link.py
+++ scrape_olx_link.py
@@ -28,84 +28,26 @@ def scrab_product_link(product):
     link = re.search(r'(?<=a\ href=")[\w\W]*?\.html', product)
     return str(link.group(0))
 
-'''
-def write_to_file(path_file, list_products):
-    """write product links in .txt file """
-    file = open(path_file, "a")
 
-    for pr in list_products:
-        link = scrab_product_link(pr)
-        # print('link', link)
-        response = get_response(link)
-        logging.debug('Work with page - ' + "[" + link + "]")
-        str_response = str(response.read().decode("utf-8"))
-        # print('str_response', str_response)
-        token = get_details.parse_token(str_response)
-        id_post = get_details.parse_id_product(str_response)
-        cookie = get_details.get_cookie(response)
-        title, show_map, date, time, number, price, content = get_details.parse_details(str_response)
-
-        # print(title)
-        # print(show_map)
-        # print(date)
-        # print(time)
-        # print(number)
-        # print(price)
-        # print(content)
-       # print(token + " " + id_post + " " + cookie)
-       # print("link - " + link)
-
-        #time.sleep(2)
-        try:
-            phone_response = get_details.get_response_phone(id_post, cookie, token)
-            phone = get_details.scrab_number(phone_response)
-            logging.debug('Parse phone - ' + "[" + phone + "]")
-            file.write(link + '|' + phone + '\n')
-        except:
-            logging.warning("Product don't have number phone - " + "[" + link + "]")
-
-
-    file.close()
-'''
-
-def write_to_bd(path_file, list_products):
+def write_to_bd(name_bd, list_products):
     """write product details in sqlite """
-    # file = open(path_file, "a")
-
     for pr in list_products:
         link = scrab_product_link(pr)
-        # print('link', link)
         response = get_response(link)
         # logging.debug('Work with page - ' + "[" + link + "]")
         str_response = str(response.read().decode("utf-8"))
-        # print('str_response', str_response)
         token = get_details.parse_token(str_response)
         id_post = get_details.parse_id_product(str_response)
         cookie = get_details.get_cookie(response)
         ad_number, title, price, date, place, content = get_details.parse_details(str_response)
-
-        # print(title)
-        # print(show_map)
-        # print(date)
-        # print(time)
-        # print(number)
-        # print(price)
-        # print(content)
-       # print(token + " " + id_post + " " + cookie)
-       # print("link - " + link)
-
         #time.sleep(2)
         try:
             phone_response = get_details.get_response_phone(id_post, cookie, token)
             phone = get_details.scrab_number(phone_response)
             # logging.debug('Parse phone - ' + "[" + phone + "]")
-            # file.write(link + '|' + phone + '\n')
-            bd_olx.insert_bd(bd_name, ad_number, title, price, date, place, phone, context)
+            bd_olx.insert_bd(name_bd, ad_number, title, price, date, place, phone, content)
         except:
             logging.warning("Product don't have number phone - " + "[" + link + "]")
-
-
-    # file.close()
 
 
 def get_max_page(html):
@@ -114,15 +56,12 @@ def get_max_page(html):
     return str(max_page.group(0))
 
 
-
 def start(url):
-    # path_file = 'product.txt' # file for result
     name_bd = 'olx_sqlite'
     count_page = 1 # start page
     response = get_response(url)
     max_page = int(get_max_page(str(response.read().decode("utf-8"))))
     bd_olx.create_bd(name_bd)
-    # print(max_page)
 
     while max_page >= count_page:
         url_page = url + '?page=' + str(count_page)
@@ -130,10 +69,8 @@ def start(url):
         logging.info('PAGE - [' + str(count_page) + ']')
         html = get_response(url_page)
         list_product = scrab_product(str(html.read().decode("utf-8")))
-        # print('list_product', list_product)
         logging.debug('Scrape list of products on page')
-        # write_to_file(path_file, list_product)
-        write_to_bd(bd_name, list_product)
+        write_to_bd(name_bd, list_product)
         count_page += 1
 
     logging.info('OK!')
