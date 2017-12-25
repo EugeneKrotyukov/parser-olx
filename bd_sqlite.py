ca904bd2b0881ac726017dd226bd449329e0ede4
EugeneKrotyukov <krotyukov@gmail.com>
2017-12-25 02:03:10 +0200

    Working variant. Use urllib.request. Get phone and ect. Write in sqllite

--- bd_sqlite.py
+++ bd_sqlite.py
@@ -12,28 +12,14 @@ def create_bd(bd_name):
         price,
         date,
         time,
+        phone,
         place,
         content)''')
 
 
-def insert_bd(bd_name, number, title, price, date, time, place, content):
+def insert_bd(bd_name, number, title, price, date, time, phone, place, content):
     conn = sqlite3.connect(bd_name)
     with conn:
         cursor = conn.cursor()
-        cursor.execute('''INSERT INTO olx_parsing (number, title, price, date, time, place, content)
-                         VALUES (?, ?, ?, ?, ?, ?, ?)''', (number, title, price, date, time, place, content))
-
-
-'''
-bd_name = 'test.sqlite'
-ad_number = '1234567890987654321'
-title = 'Супер заголовок'
-price = '2345'
-date = '12 декабря 2017'
-place = 'Одесса МАлиновский р-н'
-content = 'здесь должно находится описание товара'
-
-create_bd(bd_name)
-
-insert_bd(bd_name, ad_number, title, price, date, place, content)
-'''
+        cursor.execute('''INSERT INTO olx_parsing (number, title, price, date, time, phone, place, content)
+                         VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (number, title, price, date, time, phone, place, content))
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import get_details
 import bd_sqlite
 import re
-import requests
+import urllib.request
+import urllib.error
 from bs4 import BeautifulSoup
 
 
@@ -10,7 +11,9 @@ from bs4 import BeautifulSoup
 
 def get_response(url):
     """get html"""
-    response = requests.get(url)
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
+    r = urllib.request.Request(url, headers={'user-agent': user_agent})
+    response = urllib.request.urlopen(r)
     return response
 
 
@@ -22,27 +25,40 @@ def get_max_page(html):
 
 def scrab_product(html):
     """get list of products on page"""
-    products = list()
+    products = []
     soup = BeautifulSoup(html, 'html.parser')
     for result in soup.findAll('table', attrs={'summary': 'Объявление'}):
         product = result.find('a').get('href')
         products.append(product)
-        # print(product)
     return products
 
 
-def token_and_id_(html):
+def get_cookie(response):
+    """get cookie"""
+    return response.getheader('Set-Cookie')
+
+
+def token_and_id(html):
     token = re.search(r"(?<=phoneToken\ =\ ')[\w\W]*?(?=';)", html)
-    # print(str(token.group(0)))
     id_product = re.search(r'(?<=ID)[\w\W]*?(?=\.html)', html)
-    # print(str(id_product.group(0)))
     return str(token.group(0)), str(id_product.group(0))
 
 
-# def parse_id_product(html):
-#    id_product = re.search(r'(?<=ID)[\w\W]*?(?=\.html)', html)
-#    # print(str(id_product.group(0)))
-#    return str(id_product.group(0))
+def get_phone(id_product, token, cookie):
+    url = 'https://www.olx.ua/ajax/misc/contact/phone/' + id_product +'/?pt=' + token
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/53.0.2785.143 Chrome/53.0.2785.143 Safari/537.36'
+    r = urllib.request.Request(url, headers={'user-agent': user_agent, 'Cookie': cookie})
+    try:
+        phone_response = urllib.request.urlopen(r)
+        str_phone_response = str(phone_response.read().decode("utf-8"))
+        if 'block' in phone_response:
+            phone = re.search(r'(?<=">)[\w\W]*?(?=<)', str_phone_response)
+        else:
+            phone = re.search(r'(?<=":")[\w\W]*?(?=")', str_phone_response)
+        phone = str(phone.group(0))
+    except urllib.error.URLError as e:
+        phone = e.reason
+    return phone
 
 
 def parse_details(html):
@@ -51,37 +67,39 @@ def parse_details(html):
     soup = b_soup.find('div', attrs={'class': 'offer-titlebox'})
     title = soup.find('h1').text.strip() # title
     place = soup.find('strong').text.strip() # place
-    time_date = soup.find('em').text.strip()[52:73] # time and date
-    time, date = time_date.split(',')
+    time_date = soup.find('em').text # time and date
+    time = re.search(r"(\d{2}:\d{2})", time_date)
+    time = str(time.group(0))
+    date = re.search(r"(\d{1,2}\s\w{3,9}\s\d{4})", time_date)
+    date = str(date.group(0))
     number = soup.find('small').text[18:] # number
     price = b_soup.find('div', attrs={'class': 'price-label'}).text.strip() # price
     content = b_soup.find('div', attrs={'id': 'textContent'}).text.strip() # content
     # link_img = b_soup.find('div', attrs={'id': 'offerdescription'})
-    # print(number, title, price, date, time, place)
     return number, title, price, date, time, place, content
-
-
-def parse_product(links):
-    """parsing product details and write in sqlite"""
-    for link in links:
-        r = get_response(link)
-        number, title, price, date, time, place, content = parse_details(r.text)
-        bd_sqlite.insert_bd(name_bd, number, title, price, date, time, place, content)
 
 
 def start(url):
     bd_sqlite.create_bd(name_bd)
     count_page = 1 # start page
     response = get_response(url)
-    max_page = int(get_max_page(response.text))
-    # print(max_page)
+    max_page = int(get_max_page(str(response.read().decode("utf-8"))))
 
     while max_page >= count_page:
         url_page = url + '?page=' + str(count_page)
-        # print(url_page)
         response_page = get_response(url_page)
-        list_product = scrab_product(response_page.text)
-        parse_product(list_product)
+        list_product = scrab_product(str(response_page.read().decode("utf-8")))
+        # parse_product(list_product)
+
+        for link in list_product:
+            r = get_response(link)
+            cookie = get_cookie(r)
+            html = str(r.read().decode("utf-8"))
+            token, id_product = token_and_id(html)
+            number, title, price, date, time, place, content = parse_details(html)
+            phone = get_phone(id_product, token, cookie)
+            bd_sqlite.insert_bd(name_bd, number, title, price, date, time, phone, place, content)
+
         count_page += 1
 
 
